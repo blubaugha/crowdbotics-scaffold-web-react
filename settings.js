7e39ffe478f53083c98152b9af7707ca4795a680
Andy Blubaugh <andy.blubaugh@leadingedje.com>
2018-05-11 10:49:49 -0400

    Crowdbotics React Web UI Scaffolding

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+    starterKits: {
+        createReactApp: 'create-react-app'
+    },
+    buildPacks: {
+        createReactApp: 'https://github.com/mars/create-react-app-buildpack.git'
+    },
+    git: {
+        initialCommitMessage: 'Crowdbotics React Web UI Scaffolding'
+    }
+};
+
+module.exports = settings;
